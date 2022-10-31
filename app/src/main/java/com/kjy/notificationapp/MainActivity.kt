5bc8c6b6e16ddaa51fa84b32236af1dc99ea073a
JaeYeon-Kim <example@example.com>
2022-11-01 05:18:00 +0900

    [feat: 알림 메시지 연동 구성]

--- app/src/main/java/com/kjy/notificationapp/MainActivity.kt
+++ app/src/main/java/com/kjy/notificationapp/MainActivity.kt
@@ -2,7 +2,9 @@ package com.kjy.notificationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +15,25 @@ class MainActivity : AppCompatActivity() {
     private val firebaseToken: TextView by lazy {
         findViewById(R.id.firebaseTokenTextView)
     }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+
+    }
+
+    private fun initFirebase() {
+        // 파이어베이스로 테스트 메시지 띄워주기
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                // 성공적일때
+                if(task.isSuccessful) {
+                    Log.d("파이어베이스 토큰" ,"${task.result}")
+                    // 성공적으로 파이어베이스의 토큰을 가져옴.
+                    firebaseToken.text = task.result
+                }
+            }
     }
 }
